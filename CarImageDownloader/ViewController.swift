41b836713ddbb5f68dae463b32b127d15d66e1bd
@Slava-Fly <school.94@bk.ru>
2024-10-04 17:09:57 +0800

    Initial Commit

---
+++ CarImageDownloader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CarImageDownloader
+//
+//  Created by User on 04.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
